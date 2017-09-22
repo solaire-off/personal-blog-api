5a87eed765c60d2ea60633b4501e8551a530c763
solaire-off <victor374288@mail.ru>
2017-09-22 18:03:15 +0300

    Create article model

---
+++ articles/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.utils import timezone
+
+class Tag(models.Model):
+    name = models.CharField(max_length=500,verbose_name='Заголовок')
+    create = models.DateTimeField(default=timezone.now,verbose_name='Дата')
+
+    class Meta:
+        ordering = ['-create']
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=500,verbose_name='Заголовок')
+    description = models.TextField(blank=True,null=True,verbose_name='Описание')
+    tags = models.ManyToManyField('Tag',blank=True,verbose_name='Теги')
+    text = models.TextField(verbose_name='Содержание')
+    created_date = models.DateTimeField(
+            default=timezone.now,verbose_name='Дата создания')
+    published_date = models.DateTimeField(
+            blank=True, null=True,verbose_name='Дата публикации')
+
+    class Meta:
+    	ordering = ['-created_date']
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+
+    def __str__(self):
+    	return self.title
+
+
